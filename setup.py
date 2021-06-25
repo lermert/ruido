ad888e6a0325b94c4021d5973f87a29ab120db5c
Laura <laura.ermert@posteo.net>
2021-06-24 17:57:37 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'ruido',
+    version = '0.0.0a0',
+    description = 'Scripts for extracting measurements from ambient noise auto- and cross-correlation data',
+    #long_description =
+    # url = 
+    author = 'L. Ermert, M. Denolle',
+    author_email  = 'lermert@uw.edu',
+    # license
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Topic :: Seismology',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords = 'Ambient seismic noise', "seismology", "Earth Sciences"
+    packages = find_packages(),
+    package_data={},
+    install_requires = [
+        "numpy",
+        "scipy",
+        "pandas",
+        "h5py",
+        "pytest",
+        "mpi4py"],
+    entry_points = {
+    },
+)
+
